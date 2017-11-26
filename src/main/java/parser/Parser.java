ca36afd7d9cbfe62c81b352136c56cdeca73becf
idrisahmed251 <idrisahmed@outlook.com>
2017-11-26 22:08:52 +0000

    minor refactoring

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -2,13 +2,9 @@ package main.java.parser;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
 
 public class Parser {
     public static void main(String[] args) throws IOException {
-        File file = new File(new File(".").getCanonicalPath()  + "\\src\\main\\java\\parser");
-        FileHandler fileHandler = new FileHandler(file);
-        //Set<File> files = FileHandler.getFilesFromWorkingDirectory(new File(".").getCanonicalPath()  + "\\src\\main\\java\\parser");
-        //FileHandler.printAllFiles(files);
+        FileHandler files = new FileHandler(new File(new File(".").getCanonicalPath()  + "\\src\\main\\java\\parser"));
     }
 }
